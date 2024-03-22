12d5afd7fed72e3acc472f2cdca7a077500955f2
Joao M. <jm@gmail.com>
2024-03-22 17:39:04 -0300

    funcoes

--- main.c
+++ main.c
@@ -1,33 +1,32 @@
 #include "tarefas.h"
 #include <stdio.h>
 
-int main(){
-    int pos = 0;
-    Tarefa tarefas[TOTAL];
-    
-    int opcao;
-    do {
-        printf("\n Menu principal\n");
-        printf("1 - Criar Tarefa\n");
-        printf("2 - deletar tarefa\n");
-        printf("3 - listar tarefas\n");
-        printf("0 - Sair\n");
-        printf("Entre com uma opcao: \n");
-        
-        int i = scanf("%d", &opcao);
-        
-        printf("Opcao escolhida: %d\n", opcao);
-        if (opcao == 1) {
-            int erro = criar(tarefas, &pos);
-        }else if (opcao == 2) {
-            int erro = deletar(tarefas, &pos);
-        }else if (opcao == 3) {
-            int erro = listar(tarefas, pos);
-        }else if (opcao == 0) {
-            printf("Sair\n");
-        }else {
-            printf("opcao invalida!\n");
-        }
-        
-    } while(opcao != 0);  
+int main() {
+  int pos = 0;
+  Tarefa tarefas[TOTAL];
+
+  int opcao;
+  do {
+    printf("\nMenu principal\n");
+    printf("1 - Criar tarefa\n");
+    printf("2 - Deletar tarefa\n");
+    printf("3 - Listar tarefas\n");
+    printf("0 - Sair\n");
+    printf("Entre com uma opcao: ");
+    int i = scanf("%d", &opcao);
+
+    printf("Opcao escolhida: %d\n", opcao);
+    if (opcao == 1) {
+      int erro = criar(tarefas, &pos);
+    } else if (opcao == 2) {
+      int erro = deletar(tarefas, &pos);
+    } else if (opcao == 3) {
+      int erro = listar(tarefas, pos);
+    } else if (opcao == 0) {
+      printf("Sair\n");
+    } else {
+      printf("opcao invalida");
+    }
+
+  } while (opcao != 0);
 }
--- tarefas.h
+++ tarefas.h
@@ -3,11 +3,11 @@
 typedef struct Tarefa {
     int prioridade;
     char descricao[300];
-    char categoria [100];
+    char categoria[100];
 } Tarefa;
 
-int criar(Tarefa tarefas[], int *pos) {}
-int deletar(Tarefa tarefas[], int *pos) {}
-int listar(Tarefa tarefas[], int pos) {}
-int salvar(Tarefa tarefas[], int total, int pos) {}
-int carregar(Tarefa tarefas[], int total, int pos) {}
+int criar(Tarefa tarefas[], int *pos);
+int deletar(Tarefa tarefas[], int *pos);
+int listar(Tarefa tarefas[], int pos);
+int salvar(Tarefa tarefas[], int total, int pos);
+int carregar(Tarefa tarefas[], int total, int pos);
