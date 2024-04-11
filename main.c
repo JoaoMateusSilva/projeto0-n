52a8ab100997c931508a221b633ea859a2a3b138
Herondsx <142829869+Herondsx@users.noreply.github.com>
2024-04-10 23:12:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,61 +1,65 @@
-#include "tarefas.h"
 #include <stdio.h>
+#include "tarefas.h"
+
+#define CATEGORIA_SIZE 100
+#define DESCRICAO_SIZE 300
 
 int main() {
-  int pos = 0;
-  Tarefa tarefas[TOTAL];
+    funcao fs[] = {criar, deletar, listar, salvar, carregar};
 
-  Erro e = carregar(tarefas, TOTAL, &pos);
-  if(e == ABRIR){
-    printf("erro ao abrir o arquivo\n");
-    pos = 0;
-  }
-  else if(e == FECHAR){
-    printf("erro ao fechar o arquivo\n");
-    pos = 0;
-  }
-  else if(e == LER){
-    printf("erro ao ler no arquivo\n");
-    pos = 0;
-  }
+    Tarefa tarefas[TOTAL];
+    int pos;
+    ERROS erro = fs[4](tarefas, &pos);
+    if (erro != OK)
+        pos = 0;
 
-  int opcao;
-  do {
-    printf("\nMenu principal\n");
-    printf("1 - Criar tarefa\n");
-    printf("2 - Deletar tarefa\n");
-    printf("3 - Listar tarefas\n");
-    printf("0 - Sair\n");
-    printf("Entre com uma opcao: ");
-    int i = scanf("%d", &opcao);
+    int opcao;
+    char categoria[CATEGORIA_SIZE];
+    char nomeArquivo[100];
 
-    printf("Opcao escolhida: %d\n", opcao);
-    if (opcao == 1) {
-      e = criar(tarefas, &pos);
-      if(e == MAX_TAREFAS)
-        printf("maximo de tarefas alcancadas\n");
-    } else if (opcao == 2) {
-      e = deletar(tarefas, &pos);
-      if(e == SEM_TAREFAS)
-        printf("sem tarefas para deletar\n");
-      else if(e == NAO_EXISTE)
-        printf("tarefa nao existe\n");
-    } else if (opcao == 3) {
-      e = listar(tarefas, pos);
-      if(e == SEM_TAREFAS)
-        printf("sem tarefas para listar\n");
-    } else if (opcao == 0) {
-      printf("Sair\n");
-      e = salvar(tarefas, TOTAL, pos);
-      if(e == ABRIR)
-        printf("erro ao abrir o arquivo\n");
-      else if(e == FECHAR)
-        printf("erro ao fechar o arquivo\n");
-      else if(e == ESCREVER)
-        printf("erro ao escrever no arquivo\n");
-    } else {
-      printf("opcao invalida");
-    }
+    do {
+        printf("\nMenu principal\n");
+        printf("1 - Criar tarefa\n");
+        printf("2 - Deletar tarefa\n");
+        printf("3 - Listar tarefas\n");
+        printf("4 - Exportar tarefas\n");
+        printf("5 - Carregar tarefas\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
 
-  } while (opcao != 0);
+        scanf("%d", &opcao);
+        clearBuffer();
+
+        switch (opcao) {
+            case 1:
+                fs[0](tarefas, &pos);
+                break;
+            case 2:
+                fs[1](tarefas, &pos);
+                break;
+            case 3:
+                printf("Entre com a categoria: ");
+                fgets(categoria, CATEGORIA_SIZE, stdin);
+                fs[2](tarefas, pos, categoria);
+                break;
+            case 4:
+                printf("Entrar com o nome do arquivo que esta exportanto:  ");
+                fgets(nomeArquivo, 100, stdin);
+                fs[3](tarefas, pos, nomeArquivo);
+                break;
+            case 5:
+                printf("Entrar com o arquivo de load: ");
+                fgets(nomeArquivo, 100, stdin);
+                fs[4](tarefas, &pos, nomeArquivo);
+                break;
+            case 0:
+                printf("Sair...\n");
+                break;
+            default:
+                printf("Opcao invalida\n");
+        }
+
+    } while (opcao != 0);
+
+    fs[3](tarefas, pos);
 }
